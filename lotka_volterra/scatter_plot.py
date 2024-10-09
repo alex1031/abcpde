14737c4df7bf030d9d724e792e5f8bdd7a321eec
Alex <alex103115@yahoo.com.au>
2024-10-09 19:14:30 +1100

    added customisation within scatter plot + different results.

--- lotka_volterra/scatter_plot.py
+++ lotka_volterra/scatter_plot.py
@@ -6,6 +6,8 @@ import os
 DISTANCE_METRIC = ["Wasserstein Distance"]
 RESULT_PATH = "./lotka_volterra/results"
 PLOT_PATH = "./lotka_volterra/plots"
+QUANTILE = ["0.1%", "1%", "5%"]
+EDGECOLORS = ["r", "g", "y"]
 
 if __name__ == "__main__":
     
@@ -16,19 +18,24 @@ if __name__ == "__main__":
     for model in models:
         fig, ax = plt.subplots(2, 3, sharex=True, sharey=True)
         ax = ax.flatten()
+        i = 0
         for idx, metric in enumerate(metrics):
             metric_path_plot = os.path.join(RESULT_PATH, model)
             posterior_path = os.path.join(metric_path_plot, metric)
             posteriors = os.listdir(posterior_path)
-            post_dict = {"a": np.array([]), "b": np.array([])}
+            # post_dict = {"a": np.array([]), "b": np.array([])}
             for posterior in posteriors:
                 path = os.path.join(posterior_path, posterior)
                 p = np.load(path)
-                post_dict["a"] = np.concatenate((post_dict["a"], p[:,:,1][:,0]))
-                post_dict["b"] = np.concatenate((post_dict["b"], p[:,:,1][:,1]))
-            ax[idx].scatter(post_dict["a"], post_dict["b"])
+                # post_dict["a"] = np.concatenate((post_dict["a"], p[:,:,1][:,0]))
+                # post_dict["b"] = np.concatenate((post_dict["b"], p[:,:,1][:,1]))
+                ax[idx].scatter(p[:,:,1][:,0],  p[:,:,1][:,1], facecolors="none", edgecolor=EDGECOLORS[i%3])
+                i += 1
             ax[idx].set_title(metric, fontsize=6)
+            ax[idx].axvline(1)
+            ax[idx].axhline(1)
         
+        fig.legend(QUANTILE, loc="lower right")
         plot_model_path = os.path.join(PLOT_PATH, model)
         if not os.path.isdir(plot_model_path):
             os.mkdir(plot_model_path)
