8a4e77a4020d25bdea8ced7fbbf0da72a55a4476
Alex <alex103115@yahoo.com.au>
2025-03-30 11:48:37 +1100

    added dataframe and modified calculate_abc_posterior + plots.

--- gaussian_plume/results_dataframe.py
+++ gaussian_plume/results_dataframe.py
@@ -5,14 +5,18 @@ import itertools
 
 # Assume the directories are correct
 
-# Results in form [[a_mean, a_median, a_lowerbound, a_upperbound], [b_mean, b_median, b_lowerbound, b_upperbound]]
-RESULTS_DIR = "./lotka_volterra/results"
-SAVE_DIR = "./lotka_volterra/dataframe"
+# Results in form [[cx_mean, cx_median, cx_lowerbound, cx_upperbound, cx_std, cx_sqerr], same for cy and s]
+# cx_sqerr used to calculate the RMSE
+
+RESULTS_DIR = "./gaussian_plume/results"
+SAVE_DIR = "./gaussian_plume/dataframe"
 SUMMARY_STATISTICS = ["Mean", "Median", "StDev", "Lower Bound", "Upper Bound", "RMSE"]
 QUANTILE = [0.05, 0.01, 0.001]
 QUANTILE_NAME = ["5%", "1%", "0.1%"]
-PARAMS = ["a", "b"]
-TRUE_VALUE = 1
+PARAMS = ["cx", "cy", "s"]
+TRUE_CX = 0.5
+TRUE_CY = 0.5
+TRUE_S = 5e-5
 
 if __name__ == "__main__":
     
@@ -20,23 +24,23 @@ if __name__ == "__main__":
     model_path = os.path.join(RESULTS_DIR, models[0])
     distances = os.listdir(model_path) # All metrics - Used as columns
 
-    permutations = list(itertools.product(models, QUANTILE_NAME, PARAMS, SUMMARY_STATISTICS))
+    permutations = list(itertools.product(models, QUANTILE_NAME, PARAMS, SUMMARY_STATISTICS)) # All the different combination of permutations
     index_perms = np.array([list(item) for item in permutations])
 
     distance_val = {} 
 
     for result in models:
-        result_path = os.path.join(RESULTS_DIR, result)
+        result_path = os.path.join(RESULTS_DIR, result) # results/Wasserstein
 
         for metric in distances:
-            metric_path = os.path.join(result_path, metric)
-            posteriors = np.zeros((2, 4))
+            metric_path = os.path.join(result_path, metric) 
+            # posteriors = np.zeros((2, 4))
         
             for q in QUANTILE:
                 posterior_path = os.path.join(metric_path, f"{q}posterior.npy")
                 post = np.load(posterior_path)
                 posterior_avg = np.mean(post[:,:,:5], axis=0) # Exclude squared error axis
-                posterior_rmse = np.sqrt(np.sum(post[:,:,5], axis=0)/post.shape[0]).reshape((2, 1)) # RMSE formula
+                posterior_rmse = np.sqrt(np.sum(post[:,:,5], axis=0)/post.shape[0]).reshape((3, 1)) # RMSE formula
                 posterior_avg = np.concatenate((posterior_avg, posterior_rmse), axis=1)
                 
                 if metric not in distance_val:
