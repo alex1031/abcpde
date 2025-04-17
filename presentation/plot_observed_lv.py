9ec3dfea674d2dc5e5a182c03e1121d15fde5066
Alex Liu <aliu0994@uni.sydney.edu.au>
2025-04-17 11:00:44 +1000

    added presentation plots.

--- presentation/plot_observed_lv.py
+++ presentation/plot_observed_lv.py
@@ -19,22 +19,32 @@ if __name__ == "__main__":
     # Plot observed
     axes[0].plot(x, true_prey, color="blue", label="Observed Prey")
     axes[0].plot(x, true_pred, color="red", label="Observed Predator")
+    axes[0].set_title("No Noise (True Solution)")
 
     for i, model in enumerate(MODELS):
         # Need to always plot the true solution
-        axes[i+1].plot(x, true_prey)
-        axes[i+1].plot(x, true_pred)
+        axes[i+1].plot(x, true_prey, label="Observed Prey")
+        axes[i+1].plot(x, true_pred, label="Observed Predator")
         for smoothing in SMOOTHING:
             obs_path = os.path.join(OBSERVED_PATH, model+"_"+smoothing+f"/{model}_{smoothing}.npy")
             obs = np.load(obs_path)
             # Plot scatter if no smoothing
             if smoothing == "no_smoothing":
-                axes[i+1].scatter(x, obs[:,0], color="blue", s=10, label="Prey w/Noise")
-                axes[i+1].scatter(x, obs[:,1], color="red", s=10, label="Predator w/Noise")
+                axes[i+1].scatter(x, obs[:,0], color="blue", s=10, label="Prey w/Noise", alpha=0.3)
+                axes[i+1].scatter(x, obs[:,1], color="red", s=10, label="Predator w/Noise", alpha=0.3)
             # Plot dotted line for smoothing
             else:
                 axes[i+1].plot(x, obs[:,0], color="blue", linestyle='--', label="Prey Spline")
                 axes[i+1].plot(x, obs[:,1], color="red", linestyle='--', label="Predator Spline")
+
+        sigma = model[1:]
+        if sigma == "linear":
+            title = "$\\varepsilon\\sim\\mathcal{{N}}(0, t^2)$"
+        else:
+            title = f"$\\varepsilon\\sim\\mathcal{{N}}(0, {sigma}^2)$"
+        axes[i+1].set_title(title)
     
+    axes[-3].legend()
+    fig.suptitle("Observed Data Under Different Noise Conditions", fontsize=16)
     save_path = os.path.join(SAVE_PATH, "lv_observed.png")
     fig.savefig(save_path)
