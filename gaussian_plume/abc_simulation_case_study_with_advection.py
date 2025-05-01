3f93f92b19fec04615bc99b7e775b51821520fa1
Alex <alex103115@yahoo.com.au>
2025-05-01 10:05:50 +1000

    added case study with advection files.

---
+++ gaussian_plume/abc_simulation_case_study_with_advection.py
@@ -0,0 +1,126 @@
+import numpy as np
+import time
+import logging
+import os
+from common.distances import wasserstein_distance, cramer_von_mises_case_study, frechet_distance
+from scipy.spatial.distance import directed_hausdorff
+
+NX = 11
+NY = 18
+LX = 1.0
+LY = 1.0
+TEND = 300
+DT = 3 # To ensure 100 time steps
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+
+def generate_solution(nx, ny, Lx, Ly, cx, cy, s): 
+    dx, dy = Lx/(nx-1), Ly/(ny-1)
+    dt = DT
+    tend = TEND
+    t = 0
+
+    cfl_x, cfl_y = cx * dt/dx, cy * dt/dy
+    diff_x, diff_y = s * dt/dx**2, s * dt/dy**2
+
+    u = np.zeros((nx+2, ny+2))
+    sol = []
+    source_x, source_y = 0, ny // 2 # Source point comes from x=0 and half of y
+    u[source_x, source_y] = 1.0 # Cocentration starts from the central peak
+    
+    while t < tend:
+        unew = u.copy()
+        sol.append(u[1:-1, 1:-1])
+
+         # Advection (Upwind Scheme)
+        unew[1:-1, 1:-1] -= cfl_x * (u[1:-1, 1:-1] - u[1:-1, :-2])
+        unew[1:-1, 1:-1] -= cfl_y * (u[1:-1, 1:-1] - u[:-2, 1:-1])
+    
+        # Diffusion (Central Differencing)
+        unew[1:-1, 1:-1] += diff_x * (u[1:-1, 2:] - 2*u[1:-1, 1:-1] + u[1:-1, :-2])
+        unew[1:-1, 1:-1] += diff_y * (u[2:, 1:-1] - 2*u[1:-1, 1:-1] + u[:-2, 1:-1])
+
+        u = unew
+        t += dt
+
+    '''
+    We transpose the axis the solution. Such that:
+    - Axis 0: x
+    - Axis 1: y
+    - Axis 2: time
+    Interpretation: For each x-grid, we have the concentration of each y-grid over time.
+    Essentially, the (ny, 1200) array represents the concentration at each y over the time.
+    So we have an array of size 1200 for each y. (A curve)
+    '''
+    return np.array(sol) 
+
+def compute_frechet_distance(sim, obs):
+    return np.mean([frechet_distance(sim, obs)])
+
+def compute_directed_hausdorff(sim, obs):
+    return np.mean([max(directed_hausdorff(sim, obs)[0], directed_hausdorff(obs, sim)[0])])
+
+def abc_simulation(observed, n=100, normalised=False): # Performs Approximate Bayesian Computation (ABC) simulation. Returns results and timing information
+    # To store overall results and all the sim times
+    results, sim_time = [], [] 
+
+    rng = np.random.default_rng()
+    cx, cy = rng.uniform(0, 5, n), rng.uniform(0, 5, n) # Moderate wind conditions.
+    s = rng.uniform(0, 1e-4, n)
+
+    for i in range(n):
+        start_time = time.time()
+        simulated = generate_solution(NX, NY, LX, LY, cx[i], cy[i], s[i])[-1] # We now compare the final state of the plume, i.e. the last iteration of the solution.
+        if normalised:
+            simulated /= np.max(simulated) # Normalise it if true
+
+        if i % 10000 == 0 or i == n-1:
+            logging.info(f"Iteration {i+1}/{n}: Simulation completed in {time.time() - start_time:.2f}s.")
+
+        # Applying Distance Metrics (Need to be changed depending on how the final result is computed, can possibly extend all the other distance metrics?)
+        ## Wasserstein Distance
+        wass_start = time.time()
+        wass = np.mean(wasserstein_distance(simulated.T.flatten(), observed.flatten()))
+        wass_time = time.time() - wass_start
+
+        ## CvMD
+        cvmd_start = time.time()
+        cvmd = np.mean(cramer_von_mises_case_study(simulated.T.flatten(), observed.flatten()))
+        cvmd_time = time.time() - cvmd_start
+
+        ## Functional Frechet - We have to calculate for each grid's concentration over time.
+        frechet_start = time.time()
+        frechet = compute_frechet_distance(simulated.T, observed)
+        frechet_time = time.time() - frechet_start
+
+        ## Hausdorff
+        hausdorff_start = time.time()
+        hausdorff = compute_directed_hausdorff(simulated.T, observed)
+        hausdorff_time = time.time() - hausdorff_start
+
+        results.append([cx[i], cy[i], s[i], wass, cvmd, frechet, hausdorff])
+        sim_time.append([wass_time, cvmd_time, frechet_time, hausdorff_time])
+
+        if i % 10000 == 0 or i == n-1:
+            logging.info(f"Iteration {i+1}/{n}: Distances computed in {time.time() - start_time:.2f}s.")
+
+    '''
+        np.mean(sim_time, axis=0): Gives us the average computation time for each distance metric 
+    '''
+    return np.array(results), np.mean(sim_time, axis=0) 
+
+def main(observed_path: str, save_path: str, save_path_sim:str, normalised=False) -> None:
+    if os.path.exists(save_path):
+        logging.info("Results already exist. Skipping computation.")
+        return
+    
+    observed_data = np.load(observed_path)
+    start_time = time.time()
+    results, sim_times = abc_simulation(observed_data, normalised=normalised)
+    logging.info(f"Total run time: {time.time() - start_time:.2f}s.")
+    np.save(save_path, results)
+    np.save(save_path_sim, sim_times)
+
+
+
---
+++ gaussian_plume/run_test_case_study_with_advection.py
@@ -0,0 +1,76 @@
+import os
+import numpy as np
+import logging
+import traceback
+from multiprocessing import Pool, cpu_count
+from typing import List, Iterator, Tuple
+from gaussian_plume.abc_simulation_case_study_with_advection import main
+import time
+
+SAVE_DIR = "./gaussian_plume/runs"
+OBSERVED_DIR = "./gaussian_plume/observed_data"
+NUM_WORKERS = 5 # Needs to be changed by the number of jobs being parallelised
+NUM_RUNS = 2 # Change this later - Number of times simulation is repeated
+
+logging.basicConfig(
+    level=logging.INFO, 
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+def generate_paths(normalised=False) -> List[Tuple[str, str, str]]:
+    """
+    Generates file paths for observed data and output files for multiple runs.
+    """
+    if normalised:
+        observed_path = os.path.join(OBSERVED_DIR, "case_study", "case_study_normalised.npy")
+        output_dir = os.path.join(SAVE_DIR, "case_study_with_advection_normalised")
+    else:    
+        observed_path = os.path.join(OBSERVED_DIR, "case_study", "case_study.npy")
+        output_dir = os.path.join(SAVE_DIR, "case_study_with_advection")
+
+    return [
+        (observed_path, os.path.join(output_dir, f"run{i+1}"), os.path.join(output_dir, f"run{i+1}_sim_time"))
+        for i in range(NUM_RUNS)
+    ]
+
+def run_simulation(args: Tuple[str, str, str]) -> None:
+    """
+    Wrapper function for `main()` to execute ABC simulation.
+    """
+
+    observed_path, output_path, time_output_path = args
+    try:
+        logging.info(f"Starting simulation for: {output_path}")
+        main(observed_path, output_path, time_output_path, normalised=True)
+        logging.info(f"Completed simulation: {output_path}")
+    except Exception as e:
+        error_message = f"Error in simulation for {output_path}: {str(e)}\n{traceback.format_exc()}"
+        logging.error(error_message)
+
+def task_callback(result: str) -> None:
+    """Logs successful task completion."""
+    logging.info(result)
+
+def error_callback(error: Exception) -> None:
+    """Logs failed task errors."""
+    logging.error(f"Task failed with error: {error}")
+
+if __name__ == "__main__":
+
+    task_args = generate_paths(normalised=True)
+
+    start_time = time.time()
+    
+    num_workers = min(cpu_count(), NUM_WORKERS)
+    with Pool(num_workers) as pool:
+        # pool.starmap_async(run_simulation, task_args, callback=custom_callback)
+        for args in task_args:
+            pool.apply_async(run_simulation, args=(args,), callback=task_callback, error_callback=error_callback)
+        pool.close()
+        pool.join()
+
+    logging.info(f"Total execution time: {time.time() - start_time:.2f} seconds")
+        
+
+
+
